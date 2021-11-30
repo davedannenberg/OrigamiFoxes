f7f969de685aac0ceea3dc9f021527252e92e754
davedannenberg <85848524+davedannenberg@users.noreply.github.com>
2021-11-30 00:47:18 -0500

    update due to ipfs change

--- NFTOrigamiFoxes.sol
+++ NFTOrigamiFoxes.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+// import "@openzeppelin/contracts/presets/ERC721PresetMinterPauserAutoId.sol";
 
 contract NFTCollectible is ERC721Enumerable, Ownable {
     using SafeMath for uint256;
@@ -13,21 +14,26 @@ contract NFTCollectible is ERC721Enumerable, Ownable {
     
     Counters.Counter private _tokenIds;
     
-    uint public constant MAX_SUPPLY = 820; // Maximum number of NFTs that can be minted
+    uint public constant MAX_SUPPLY = 760; // Maximum number of NFTs that can be minted
     // uint public constant PRICE = 0.0000001 ether; // Free distribution to bootcamp students
     uint public constant MAX_PER_MINT = 1; // Only one NFT per mint
     
     string public baseTokenURI;
-    
-    // Constructor to set the name and symbol of NFT collection. 
+    string public completeURI;
+    // Constructor to set the name and symbol of NFT collection.
+    // event CreateFox(uint256 indexed id);
     constructor(string memory baseURI) ERC721("Origami Foxes", "OF") {
+        // completeURI = baseURI + ".png";
         setBaseURI(baseURI);
     }
     
+    // function baseTokenURI() public view returns (string memory) {
+    // return "ipfs://QmNWF1pqcc49fRhXysj7TNZ6SBg7gVDcQKLjg25JRMcvGX/";
+    // }
     // Mint NFTs for free to bootcamp students
     function reserveNFTs() public onlyOwner {
         uint totalMinted = _tokenIds.current();
-        uint freeFoxes = 820; // Minting 820 NFTs for free
+        uint freeFoxes = 760; // Minting 820 NFTs for free
 
         require(totalMinted.add(freeFoxes) < MAX_SUPPLY, "Not enough NFTs left to reserve"); 
 
@@ -61,9 +67,9 @@ contract NFTCollectible is ERC721Enumerable, Ownable {
     }
 
     //Returns URI for collection
-    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-        return baseTokenURI;
-    }
+    // function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+    //     return baseTokenURI;
+    // }
     
     // Function to mint NFT from collection
     function _mintSingleNFT() private {
